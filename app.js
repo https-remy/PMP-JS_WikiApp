053a581de4841b8dad73b491d94226d6997fa7fd
z0opreme <remy.engassac@icloud.com>
2024-04-30 21:48:55 +0200

    html and css done

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
 // API ENDPOINT : `https://en.wikipedia.org/w/api.php?action=query&list=search&format=json&origin=*&srlimit=20&srsearch=${searchInput}`
+
+const form = document.querySelector("form");
+const searchInput = document.querySelector("#search-input");
+
+form.addEventListener("submit", (e) => {
+	e.preventDefault();
+});
